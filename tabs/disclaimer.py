6008a11a754601f2f2ba2fbdbf0782753a65a9cb
zhanghaohan <nullland1027@outlook.com>
2025-06-21 19:46:28 +0800

    Split disclaimer.py layout into 2 cols

--- tabs/disclaimer.py
+++ tabs/disclaimer.py
@@ -4,18 +4,29 @@ def show_disclaimer():
     """显示免责声明页面内容"""
     st.title("Disclaimer")
 
-    st.markdown("""
-    ## Disclaimer
+    left, right = st.columns([1, 1])
 
-    This non-profit server and its associated data and services are intended solely for academic and research purposes.
-     The Protein Structure and Bioinformatics Group at Lund University endeavors to provide high-quality tools and datasets;
-      however, we do not accept responsibility for the outcomes derived from the use of this server, including any results, analyses, or interpretations.
+    with left:
 
-    ## Liability
+        st.markdown("""
+        ## Disclaimer
+    
+        This non-profit server and its associated data and services are intended solely for academic and research purposes.
+         The Protein Structure and Bioinformatics Group at Lund University endeavors to provide high-quality tools and datasets;
+          however, we do not accept responsibility for the outcomes derived from the use of this server, including any results, analyses, or interpretations.
+          """)
 
-    While every effort has been made to ensure the accuracy and timeliness of the information provided on this site, 
-    no warranties—explicit or implied—are given regarding its correctness, completeness, 
-    or suitability for any specific purpose. 
-    Users agree that Lund University and its staff or affiliates bear no liability for any direct, indirect, incidental, or consequential damages arising from the use of this service. 
-    Use of any process, method, or product based on this site’s content is at the user’s own risk.
-    """)
+    with right:
+
+        st.markdown(
+            """
+                ## Liability
+    
+                While every effort has been made to ensure the accuracy and timeliness of the information provided on this site, 
+                no warranties—explicit or implied—are given regarding its correctness, completeness, 
+                or suitability for any specific purpose. 
+                Users agree that Lund University and its staff or affiliates bear no liability for any direct, indirect, incidental, or consequential damages arising from the use of this service. 
+                Use of any process, method, or product based on this site’s content is at the user’s own risk.
+                """
+        )
+
